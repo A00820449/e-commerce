06058ec1b38ed9b80234fbcdd5dab1cda120f7f6
Miguel Tornero <a00820449@itesm.mx>
2023-01-11 00:00:18 -0600

    removed sign in links

--- src/modules/views/AppAppBar.js
+++ src/modules/views/AppAppBar.js
@@ -4,12 +4,6 @@ import Link from '@mui/material/Link';
 import AppBar from '../components/AppBar';
 import Toolbar from '../components/Toolbar';
 
-const rightLink = {
-  fontSize: 16,
-  color: 'common.white',
-  ml: 3,
-};
-
 function AppAppBar() {
   return (
     <div>
@@ -25,25 +19,7 @@ function AppAppBar() {
           >
             {'onepirate'}
           </Link>
-          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
-            <Link
-              color="inherit"
-              variant="h6"
-              underline="none"
-              href="/premium-themes/onepirate/sign-in/"
-              sx={rightLink}
-            >
-              {'Sign In'}
-            </Link>
-            <Link
-              variant="h6"
-              underline="none"
-              href="/premium-themes/onepirate/sign-up/"
-              sx={{ ...rightLink, color: 'secondary.main' }}
-            >
-              {'Sign Up'}
-            </Link>
-          </Box>
+          <Box sx={{ flex: 1}} />
         </Toolbar>
       </AppBar>
       <Toolbar />
